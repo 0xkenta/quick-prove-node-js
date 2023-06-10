9e65aa401c46d5df2185113738d200c009192dbb
0xkenta <kenta0558kiichi614@gmail.com>
2023-06-11 01:22:59 +0900

    test

--- controllers/utils.ts
+++ controllers/utils.ts
@@ -2,23 +2,22 @@ import { Response, NextFunction } from 'express';
 import { Schema } from 'mongoose';
 
 const errorHandler = (res: Response, next: NextFunction, status: number, errMessage: string) => {
-  const err = new Error(errMessage);
-  res.status(status);
-  next(err);
+    const err = new Error(errMessage);
+    res.status(status);
+    next(err);
 };
 
 const isValidHttpURL = (url: string) => {
-  try {
-    const newUrl = new URL(url);
-    return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
-  } catch (err) {
-    return false;
-  }
+    try {
+        const newUrl = new URL(url);
+        return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
+    } catch (err) {
+        return false;
+    }
 };
 
 const isInWatchlist = (watchlist: Schema.Types.ObjectId[], newId: Schema.Types.ObjectId): boolean => {
-  return watchlist.includes(newId);
-  console.log("test")
+    return watchlist.includes(newId);
 };
 
 export { errorHandler, isValidHttpURL, isInWatchlist };
